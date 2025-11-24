9dcae23152709755da27646076ebeb69376388a4
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-25 01:47:23 +0200

    fix: refactor search function to optimize memory usage by removing unnecessary vector ID accumulation

--- IVF.py
+++ IVF.py
@@ -172,26 +172,26 @@ def search(vec_db, query_vector, k=5, batch_size=500):
     # lengths_array = np.frombuffer(mm, dtype=np.uint32, count=n_clusters, offset=lengths_offset)
 
     candidates = []
-    all_vec_ids = []
+    # all_vec_ids = []
 
     for cid in selected_centroids:
         vec_ids = load_cluster_ids(filename, cid, lengths_array, ids_offset)
-        all_vec_ids.extend(vec_ids)
+    #     all_vec_ids.extend(vec_ids)
     
-    all_vec_ids = np.sort(np.array(all_vec_ids, dtype=np.int64))
+    # all_vec_ids = np.sort(np.array(all_vec_ids, dtype=np.int64))
 
-    # Process in batches to limit memory usage
-    for start in range(0, len(all_vec_ids), batch_size):
-        vec_ids = all_vec_ids[start:start+batch_size]
+    # # Process in batches to limit memory usage
+    # for start in range(0, len(all_vec_ids), batch_size):
+    #     vec_ids = all_vec_ids[start:start+batch_size]
 
-        # for start in range(0, len(vec_ids), batch_size):
-        #     vec_ids_batch = vec_ids[start:start+batch_size]
+        for start in range(0, len(vec_ids), batch_size):
+            vec_ids_batch = vec_ids[start:start+batch_size]
 
-        vecs = vec_db.get_rows(vec_ids)
+        vecs = vec_db.get_rows(vec_ids_batch)
 
         vecs = vecs / (np.linalg.norm(vecs, axis=1, keepdims=True) + 1e-12)
 
-        for vid, vec in zip(vec_ids, vecs):
+        for vid, vec in zip(vec_ids_batch, vecs):
             score = cal_score(query_vector, vec)
 
             item = (score, -vid)
