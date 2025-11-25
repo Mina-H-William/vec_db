8189ef5230e5592cec8f51ae2201e0dab7c082ad
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-25 02:40:17 +0200

    fix: ensure all_vec_ids is converted to a numpy array before sorting for consistency

--- IVF.py
+++ IVF.py
@@ -176,7 +176,7 @@ def search(vec_db, query_vector, k=5, batch_size=500):
         vec_ids = load_cluster_ids(filename, cid, lengths_array, ids_offset)
         all_vec_ids.extend(vec_ids)
     
-    all_vec_ids = np.sort(all_vec_ids)
+    all_vec_ids = np.sort(np.array(all_vec_ids, dtype=np.uint32))
 
     # Process in batches to limit memory usage
     for start in range(0, len(all_vec_ids), batch_size):
