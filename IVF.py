0943e8e9c1a244a598610f7849d966109310652f
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-25 02:31:51 +0200

    fix: update data type for all_vec_ids and adjust scoring logic in search function

--- IVF.py
+++ IVF.py
@@ -176,7 +176,7 @@ def search(vec_db, query_vector, k=5, batch_size=500):
         vec_ids = load_cluster_ids(filename, cid, lengths_array, ids_offset)
         all_vec_ids.extend(vec_ids)
     
-    all_vec_ids = np.sort(np.array(all_vec_ids, dtype=np.int64))
+    all_vec_ids = np.sort(np.array(all_vec_ids, dtype=np.uint32))
 
     # Process in batches to limit memory usage
     for start in range(0, len(all_vec_ids), batch_size):
@@ -189,16 +189,16 @@ def search(vec_db, query_vector, k=5, batch_size=500):
         for vid, vec in zip(vec_ids, vecs):
             score = cal_score(query_vector, vec)
 
-            item = (score, -vid)
+            item = (-score, vid)
 
             if len(candidates) < k:
                 heapq.heappush(candidates, item)
             else:
-                if item > candidates[0]:
+                if item < candidates[0]:
                     heapq.heappushpop(candidates, item)
 
     # ---- 5. Final results ----
-    results = [(score, -vid) for score, vid in candidates]
+    results = [(-score, vid) for score, vid in candidates]
     results.sort(key=lambda x: (x[0], x[1]))  # sort by score then ID
 
     return [vid for _, vid in results]
