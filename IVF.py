2b13334292a63659cb07c40564e2bbde79874cda
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-25 19:59:15 +0200

    fix: update main search function to increase batch size for centroids to optimize performance

--- IVF.py
+++ IVF.py
@@ -188,7 +188,7 @@ def get_nearest_k_vectors(vec_db, query_vector, all_vec_ids, k, batch_size):
 
 ######################## Main search function ################
 
-def search(vec_db, query_vector, k=5, batch_size_for_centroids=500, batch_size_for_vectors=100):
+def search(vec_db, query_vector, k=5, batch_size_for_centroids=1000, batch_size_for_vectors=112):
     filename = vec_db.index_path
     query_vector = query_vector / (np.linalg.norm(query_vector) + 1e-12)
 
