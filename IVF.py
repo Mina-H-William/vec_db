e7b72879f06f4bbdca115cc96bb2ac6db7b874f7
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-29 00:30:25 +0200

    fix: update default batch sizes in search function for improved performance

--- IVF.py
+++ IVF.py
@@ -307,7 +307,7 @@ def get_nearest_k_vectors(vec_db, query_vector, all_vec_ids, k, batch_size):
 ######################## Main search function ################
 
 def search(vec_db, query_vector, k=5, 
-           batch_size_for_centroids=640, batch_size_for_vectors=16):
+           batch_size_for_centroids=800, batch_size_for_vectors=32):
 
     filename = vec_db.index_path
     query_vector = query_vector / (np.linalg.norm(query_vector) + 1e-12)
