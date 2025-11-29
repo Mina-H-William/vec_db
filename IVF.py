fix: adjust n_probe calculation and optimize vector ID collection in search function

IVF.py
@@ -202,25 +202,20 @@ def search(vec_db, query_vector, k=5, batch_size_for_centroids=1008, batch_size_
         f.seek(lengths_offset)
         lengths_array = np.frombuffer(f.read(n_clusters * 4), dtype=np.uint32)
 
-    n_probe = ((n_clusters // 1000))
+    n_probe = 5 + ((n_clusters // 1000))
 
     # ---- 2. Find nearest centroids ----
     selected_centroids = get_nearest_centroids(filename, query_vector, n_probe, batch_size_for_centroids, n_clusters, dim, centroid_offset)
 
     # ---- 3. Search actual vectors in selected clusters ----
 
-    total = lengths_array[selected_centroids].sum().astype(np.uint32)
-    all_vec_ids = np.empty(total, dtype=np.uint32)
-
-    # Fill buffer efficiently
-    pos = 0
+    all_vec_ids = []
     for cid in selected_centroids:
         vec_ids = load_cluster_ids(filename, cid, lengths_array, ids_offset)
-        L = len(vec_ids)
-        all_vec_ids[pos:pos+L] = vec_ids
-        pos += L
+        all_vec_ids.extend(vec_ids)
 
     # Sort once, fast in C
+    all_vec_ids = np.array(all_vec_ids, dtype=np.uint32)
     all_vec_ids.sort()
 
     # ---- 4. Get nearest k vectors among candidates ----