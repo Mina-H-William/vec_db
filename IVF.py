67c55e427c50a76e6d46bc2e175e54feda91829e
MHW <118685507+Monwilliam@users.noreply.github.com>
2025-11-29 21:41:30 +0200

    fix: refactor get_nearest_k_vectors to simplify candidate scoring and return structure

--- IVF.py
+++ IVF.py
@@ -177,7 +177,30 @@ def get_nearest_centroids(filename, query_vector, n_probe, batch_size, n_cluster
     return np.argpartition(-scores, n_probe-1)[:n_probe]
 
 def get_nearest_k_vectors(vec_db, query_vector, all_vec_ids, k, batch_size):
-    candidates = []
+    # candidates = []
+
+    # # Process in batches to limit memory usage
+    # for start in range(0, len(all_vec_ids), batch_size):
+    #     vec_ids = all_vec_ids[start:start+batch_size]
+
+    #     vecs = vec_db.get_rows(vec_ids)
+
+    #     vecs = vecs / (np.linalg.norm(vecs, axis=1, keepdims=True) + 1e-12)
+
+    #     scores = vecs @ query_vector
+
+    #     for vid, score in zip(vec_ids, scores):
+    #         item = (score, vid)
+
+    #         if len(candidates) < k:
+    #             heapq.heappush(candidates, item)
+    #         else:
+    #             if item[0] > candidates[0][0] or (item[0] == candidates[0][0] and item[1] < candidates[0][1]):
+    #                 heapq.heappushpop(candidates, item)
+
+    # return candidates
+
+    scores = []
 
     # Process in batches to limit memory usage
     for start in range(0, len(all_vec_ids), batch_size):
@@ -187,18 +210,9 @@ def get_nearest_k_vectors(vec_db, query_vector, all_vec_ids, k, batch_size):
 
         vecs = vecs / (np.linalg.norm(vecs, axis=1, keepdims=True) + 1e-12)
 
-        scores = vecs @ query_vector
+        scores.extend(vecs @ query_vector)
 
-        for vid, score in zip(vec_ids, scores):
-            item = (score, vid)
-
-            if len(candidates) < k:
-                heapq.heappush(candidates, item)
-            else:
-                if item[0] > candidates[0][0] or (item[0] == candidates[0][0] and item[1] < candidates[0][1]):
-                    heapq.heappushpop(candidates, item)
-
-    return candidates
+    return [(score, vid) for vid, score in zip(all_vec_ids, scores)]
 
 ######################## Main search function ################
 
