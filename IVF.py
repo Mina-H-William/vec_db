9e4a26bd45255dc2fb00c617b94876dea43eef52
Ashraf-Bahy <ashrafbahy286@gmail.com>
2025-11-30 05:09:10 +0200

    8+n/5

--- IVF.py
+++ IVF.py
@@ -193,7 +193,7 @@ def search(vec_db, query_vector, k=5, batch_size_for_centroids=2000, batch_size_
         f.seek(lengths_offset)
         lengths_array = np.frombuffer(f.read(n_clusters * 4), dtype=np.uint32)
 
-    n_probe = 7 + (n_clusters // 3000)
+    n_probe = 8 + (n_clusters // 5000)
 
     # ---- 2. Find nearest centroids ----
     selected_centroids = get_nearest_centroids(filename, query_vector, n_probe, batch_size_for_centroids, n_clusters, dim, centroid_offset)
